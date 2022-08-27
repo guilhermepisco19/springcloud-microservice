04e69a118b441f6da11d4671021ef4f02f31df83
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2022-08-27 15:09:57 +0100

    Feign Client with Eureka

--- student-service/src/main/java/com/guilhermepisco/studentservice/feignclients/AddressFeignClient.java
+++ student-service/src/main/java/com/guilhermepisco/studentservice/feignclients/AddressFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.guilhermepisco.studentservice.entity.Address;
 
-@FeignClient(url = "${address.service.url}", value = "address-feign-client", path = "/api/address")
+@FeignClient(value = "address-service", path = "/api/address")
 public interface AddressFeignClient {
 	
 	@GetMapping("/{id}")
